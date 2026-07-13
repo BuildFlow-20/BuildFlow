a771015c46b201732f0811ed07b34e282b967d0e
BuildFlow-20 <jayx.dudley@gmail.com>
2026-07-13 18:57:55 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,3 +71,26 @@ if (themeToggle) {
         document.body.classList.toggle("dark-mode");
     });
 }
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (!topBtn) return;
+
+    if (
+        document.body.scrollTop > 300 ||
+        document.documentElement.scrollTop > 300
+    ) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+if (topBtn) {
+    topBtn.onclick = function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+}
